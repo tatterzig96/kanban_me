eb4078f52bec9e1c2cd19f3e3813cb0b0c37ad16
Mark Tattersall <marktatts@gmail.com>
2011-06-07 22:11:16 -0500

    Project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KanbanMe::Application.config.secret_token = '272abcb1ca206927c7bc727212d90132adae4ec6994f389da5c7273032185e1b3f5895f6b2d955a116d0eece8c654dda09b51161e6eddce139a3eb6170f7ef96'
